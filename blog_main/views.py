817914ab86832ce4163e060ea7b751495ba3b05d
far-hana5 <farhanahussain5730532@gmail.com>
2026-01-29 17:18:39 +0600

    Featured post

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from blogs.models import Category
+from blogs.models import Category,Blog
 
 
 def home(request):
     categories=Category.objects.all()
+    featured_post=Blog.objects.filter(is_featured=True).order_by('updated_at')
+    
     context={
         'categories':categories,
+        'featured_posts':featured_post,
     }
     print(categories)
     return render(request,'home.html',context)
