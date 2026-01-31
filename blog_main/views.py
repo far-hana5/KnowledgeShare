7bd357ed7c9e3bb4681bb4877f70f1d0ab3f4d8d
far-hana5 <farhanahussain5730532@gmail.com>
2026-01-31 15:57:29 +0600

    posts

--- blog_main/views.py
+++ blog_main/views.py
@@ -5,11 +5,13 @@ from blogs.models import Category,Blog
 
 def home(request):
     categories=Category.objects.all()
-    featured_post=Blog.objects.filter(is_featured=True).order_by('updated_at')
+    featured_post=Blog.objects.filter(is_featured=True,status='Published').order_by('updated_at')
+    posts=Blog.objects.filter(is_featured=False,status='Published')
     
     context={
         'categories':categories,
         'featured_posts':featured_post,
+        'posts':posts,
     }
     print(categories)
     return render(request,'home.html',context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Blog, Category
 
-# Create your views here.
+def posts_by_category(request, category_id):
+    try:
+        category = Category.objects.get(pk=category_id)
+    except Category.DoesNotExist:
+        return redirect('home')   
+
+    posts = Blog.objects.filter(status='Published', category=category)
+
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'posts_by_category.html', context)
